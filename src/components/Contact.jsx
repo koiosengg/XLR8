bc002a949d9c50172695b0e2b102455752c654d6
Panth Shah <panthshah242@gmail.com>
2024-06-30 16:31:12 +0530

    updated p-style.css and Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,193 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="p-contact-main">
+        <div className="p-get-in-touch">
+          <div className="p-get-in-touch-text">
+            <h1>Get in touch</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Facilisis adipiscing eget
+              pulvinar at. Tortor consectetur neque.
+            </p>
+          </div>
+          <div className="p-checkbox">
+            <div className="p-checkbox-cover">
+              <div className="p-rectangle-cover">
+                <div className="p-rectangle"></div>
+              </div>
+              <p>Lorem ipsum dolor amit lorem ipsum dolor amit </p>
+            </div>
+            <div className="p-checkbox-cover">
+              <div className="p-rectangle-cover">
+                <div className="p-rectangle"></div>
+              </div>
+              <p>Lorem ipsum dolor amit lorem ipsum dolor amit </p>
+            </div>
+            <div className="p-checkbox-cover">
+              <div className="p-rectangle-cover">
+                <div className="p-rectangle"></div>
+              </div>
+              <p>Lorem ipsum dolor amit lorem ipsum dolor amit </p>
+            </div>
+          </div>
+          <div className="p-social-icons">
+            <div className="facebook">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <path
+                  d="M18 2H15C13.6739 2 12.4021 2.52678 11.4645 3.46447C10.5268 4.40215 10 5.67392 10 7V10H7V14H10V22H14V14H17L18 10H14V7C14 6.73478 14.1054 6.48043 14.2929 6.29289C14.4804 6.10536 14.7348 6 15 6H18V2Z"
+                  stroke="#D1D1D1"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+            <div className="linkedin">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <path
+                  d="M16 8C17.5913 8 19.1174 8.63214 20.2426 9.75736C21.3679 10.8826 22 12.4087 22 14V21H18V14C18 13.4696 17.7893 12.9609 17.4142 12.5858C17.0391 12.2107 16.5304 12 16 12C15.4696 12 14.9609 12.2107 14.5858 12.5858C14.2107 12.9609 14 13.4696 14 14V21H10V14C10 12.4087 10.6321 10.8826 11.7574 9.75736C12.8826 8.63214 14.4087 8 16 8Z"
+                  stroke="#D1D1D1"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M6 9H2V21H6V9Z"
+                  stroke="#D1D1D1"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M4 6C5.10457 6 6 5.10457 6 4C6 2.89543 5.10457 2 4 2C2.89543 2 2 2.89543 2 4C2 5.10457 2.89543 6 4 6Z"
+                  stroke="#D1D1D1"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="p-form">
+          <form action="" method="post">
+            <div className="p-form-top">
+              <div className="name-div">
+                <div className="firstName">
+                  <p>First Name</p>
+                  <input type="text" />
+                </div>
+                <div className="lastName">
+                  <p>Last Name</p>
+                  <input type="text" />
+                </div>
+              </div>
+              <div className="name-div">
+                <div className="firstName">
+                  <p>Phone</p>
+                  <input type="text" />
+                </div>
+                <div className="lastName">
+                  <p>Email</p>
+                  <input type="text" />
+                </div>
+              </div>
+              <div className="p-enquire-about">
+                <p className="p-enquire-title">Enquire About</p>
+                <div className="p-checkbox-grid">
+                  <div className="p-checkbox-top">
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Interested in buying</p>
+                    </div>
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Products/Services</p>
+                    </div>
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Feedback</p>
+                    </div>
+                  </div>
+                  <div className="p-checkbox-btm">
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Get Quotation</p>
+                    </div>
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Need Assistance</p>
+                    </div>
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Others</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="p-enquire-about">
+                <p className="p-enquire-title">Products</p>
+                <div className="p-checkbox-grid">
+                  <div className="p-checkbox-top">
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Product 1</p>
+                    </div>
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Product 2</p>
+                    </div>
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Product 3</p>
+                    </div>
+                  </div>
+                  <div className="p-checkbox-btm">
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Product 4</p>
+                    </div>
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Product 5</p>
+                    </div>
+                    <div className="p-checkbox-main">
+                      <input type="checkbox" className="checkbox-input" />
+                      <p>Product 6</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="p-textarea-container">
+                <p>Message</p>
+                <textarea name="" id="" rows={8}></textarea>
+              </div>
+            </div>
+            <div className="p-form-submit">
+              <button type="submit" className="p-submit">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
